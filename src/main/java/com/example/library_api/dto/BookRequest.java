7ce13a4f519f9ba4fcef153f5ee2007d66eb3a01
Mia <amelia.decobert@gmail.com>
2026-07-10 18:01:03 +0200

    refactor: update Book Request & Response

--- src/main/java/com/example/library_api/dto/BookRequest.java
+++ src/main/java/com/example/library_api/dto/BookRequest.java
@@ -3,5 +3,9 @@ package com.example.library_api.dto;
 import jakarta.validation.constraints.NotBlank;
 
 // Use NotBlank validation
-public record BookRequest(@NotBlank(message = "Title can not be empty") String title) {
+public record BookRequest(
+        @NotBlank(message = "Title can not be empty") String title,
+        @NotBlank(message = "Author can not be empty") String author,
+        String isbn,
+        Integer publicationYear) {
 }
--- src/main/java/com/example/library_api/dto/BookResponse.java
+++ src/main/java/com/example/library_api/dto/BookResponse.java
@@ -1,4 +1,9 @@
 package com.example.library_api.dto;
 
-public record BookResponse(Long id, String title) {
+public record BookResponse(
+        Long id,
+        String title,
+        String author,
+        String isbn,
+        Integer publicationYear) {
 }
