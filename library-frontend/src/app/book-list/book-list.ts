bf47c229d0b6d373298645d708a7aeefe99f8c87
Mia <amelia.decobert@gmail.com>
2026-08-14 15:30:27 +0200

    feat: update Book List Component & html

--- library-frontend/src/app/book-list/book-list.ts
+++ library-frontend/src/app/book-list/book-list.ts
@@ -1,9 +1,32 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
+import {RouterLink} from '@angular/router';
+import {BookService} from '../services/BookService';
+import {Book} from '../models/book.model';
 
 @Component({
   selector: 'app-book-list',
-  imports: [],
+  standalone: true,
+  imports: [RouterLink],
   templateUrl: './book-list.html',
   styleUrl: './book-list.css',
 })
-export class BookList {}
+export class BookList implements OnInit {
+  private bookService = inject(BookService);
+
+  books: Book[] = [];
+  loading = true;
+
+  ngOnInit(): void {
+    this.bookService.getBooks().subscribe({
+      next: (response) => {
+        this.books = response.content;
+        this.loading = false;
+      },
+      error: (err) => {
+        console.log('Erreur lors du chargement des livres', err);
+
+        this.loading = false;
+      }
+    });
+  }
+}
