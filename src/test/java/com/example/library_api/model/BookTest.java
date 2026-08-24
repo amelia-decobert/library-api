b522a682e8ccd68e53cc3f34cdc1441a416570cc
Mia <amelia.decobert@gmail.com>
2026-08-24 15:03:27 +0200

    test: intentional fail CI

--- src/test/java/com/example/library_api/model/BookTest.java
+++ src/test/java/com/example/library_api/model/BookTest.java
@@ -59,7 +59,7 @@ class BookTest {
         book.setPublicationYear(1949);
 
         // ASSERT
-        assertEquals("1984", book.getTitle());
+        assertEquals("wrong title", book.getTitle());
         assertEquals(1949, book.getPublicationYear());
     }
 
